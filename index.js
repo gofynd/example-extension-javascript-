d818fd6967262ee01456d72061c59a4ebdf78e53
Meet Koriya <meetkoriya@gofynd.com>
2023-04-24 15:17:37 +0530

    updated .env file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 require("dotenv").config();
 require('./app/common/redis.init');
 const app = require("./app/server");
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
