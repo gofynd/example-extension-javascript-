b9008159ae161c00cf10b8dda35e86cfd94fedbd
jinalviranii <jinalvirani@gofynd.com>
2024-08-27 17:09:02 +0530

    Remoce util, guard, service folders

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import vue from '@vitejs/plugin-vue'
-import { config as loadEnv } from 'dotenv';
-import path from 'path';
-loadEnv({ path: path.resolve(__dirname,'../', '.env') });
 
 const proxyOptions = {
   target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
@@ -36,6 +33,9 @@ if (host === 'localhost') {
 export default defineConfig({
   root: dirname(fileURLToPath(import.meta.url)),
   plugins: [vue()],
+  build: {
+    outDir: 'public/dist'
+  },
   server: {
     host: 'localhost',
     port: process.env.FRONTEND_PORT,
